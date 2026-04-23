d41eaada87813c996f8bb5ff12bacdb9a6e66253
Carlos Josué Reyes Delgado <carlos.josue.reyes@clases.edu.sv>
2026-04-23 09:08:25 -0600

    guia-javascript-basico
    Ejercicios prácticos realizados en el editor VS Code.

---
+++ guia-javascript-basico/ejercicio1.js
@@ -0,0 +1,6 @@
+// Declara tus variables aquí
+const nombre = "Carlos Josué Reyes Delgado";
+let edad = 28;
+
+// Imprime el mensaje de presentación
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`);
